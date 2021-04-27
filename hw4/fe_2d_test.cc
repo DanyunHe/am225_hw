2a264092272a3c497d57c1b88ede97120b4631a5
DanyunHe <danyunhe@g.harvard.edu>
2021-04-26 21:25:34 -0400

    update code

---
+++ hw4/fe_2d_test.cc
@@ -0,0 +1,20 @@
+#include <cmath>
+
+#include "bilinear_2d_fe.hh"
+
+int main() {
+
+    // Construct the finite-element class
+    bilinear_2d_fe bf(32);
+    
+    // Initialize the source function 
+    bf.init();
+
+    // Optional command to print the matrix in text form
+    //cf.print_matrix();
+
+    // Solve the finite-element problem using the conjugate gradient method
+    bf.solve();
+    //bf.print();
+    printf("%g %g\n",bf.h,bf.l2_norm_mms());
+}
